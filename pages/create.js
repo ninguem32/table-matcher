01a999b1877aa446f872966266e75356559b7a61
ninguem32 <67392860+ninguem32@users.noreply.github.com>
2021-07-14 17:14:22 -0300

    Add files via upload

--- pages/create.js
+++ pages/create.js
@@ -1,35 +1,11 @@
+/* eslint-disable @next/next/no-css-tags */
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import Container from 'react-bootstrap/Container';
+import Container from 'react-bootstrap/Container'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
- import {
-        withScriptjs,
-        withGoogleMap,
-        GoogleMap,
-        Marker
-      } from "react-google-maps";
-    
+import GoogleMapReact from 'google-map-react';
 
 export default function home() {
-  
-      const defaultCenter = { lat: 0, lng: 0 };
-      const defaultOptions = { scrollwheel: false };
-      
-      const RegularMap = withScriptjs(
-        withGoogleMap(props => {
-          return(<GoogleMap
-            defaultZoom={2}
-            defaultCenter={ props.defaultCenter || defaultCenter}
-            defaultOptions={ defaultOptions }
-            onClick={props.onClick}
-          ></GoogleMap>)
-        })
-      );
-      
-      const loadingElementStyle = { height: '100%' };
-      const containerElementStyle = { height: '280px' };
-      const mapElementStyle = { height: '100%' };
-      
     return (
       <>
         <head>
@@ -66,14 +42,17 @@ export default function home() {
               <Form.Label>Address</Form.Label>
               <Form.Control type="text" placeholder="410 Bagby St, Houston, TX 77002, USA" />
             </Form.Group>
-            <RegularMap
-                googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyAhLm1bYFxZqeZqdygReGS3mC9jt2TJ0SU"
-                loadingElement={<div style={ loadingElementStyle } />}
-                containerElement={<div style={ containerElementStyle } />}
-                mapElement={<div style={ mapElementStyle } />}
-                onClick={(e)=>{coordinades={lng:e.latLng.lng(),lat:e.latLng.lat()}}}
-                defaultCenter={window.Geolocation}
-                />
+            <div style={{ height: '50vh', width: '100%' }}>
+              <GoogleMapReact
+                bootstrapURLKeys={{ key:'AIzaSyAhLm1bYFxZqeZqdygReGS3mC9jt2TJ0SU'}}
+                defaultCenter={ {
+                  lat: 0,
+                  lng: 0
+                }}
+                onClick={(e)=>{console.log(e)}}
+                defaultZoom={2}
+              />
+            </div>
             <Button variant="secondary" type="secondary">
             Submit
             </Button>
